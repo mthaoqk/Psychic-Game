22a5bcadc4058b445aea1b6a7333e8efdca9b6e5
Mthao09 <mthaoqk@users.noreply.github.com>
2018-07-08 17:11:13 -0500

    fixed logic, scoring still needs to be fix after win prompt

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,84 @@
-var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-//lowercase
-var Wins = 0;
-var Losses = 0;
-var Guesses = 20;
-var GuessesSoFar = [];
-var compGuess = letter[Math.floor(Math.random() * letter.length)];
-console.log("Computer's Guess: " + compGuess);
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var guessedLetters = [];
+
+var letterToGuess = null;
+
+var guessesLeft = 20;
+
+var wins = 0;
+var losses = 0;
 
 
-// when a key is pressed
-document.onkeyup = function (keyInput) {
-    var userGuess = keyInput.key;
-    // if the key that the user pressed is a letter...
-    if (letter.includes(userGuess)) {
 
-        // check if users' key is the same as the computers guess
-        if (userGuess === compGuess) {
-            Wins++
-            compGuess = letter[Math.floor(Math.random() * letter.length)];
-            console.log("Computer's Guess: " + compGuess)
-        }
-        if (Wins >= 3) {
-            alert("Winner is you!")
-            document.location.reload();
-            
-        }
-        else {
-            Guesses--;
-            console.log(userGuess + " is wrong");
+var updateGuessesLeft = function () {
 
-            if (Guesses < 1){
-                alert("Take the L!" ) + Losses++;
-                document.location.reload();
-            }
-            // else (Losses > 5)
-            // document.location.reload();
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+};
 
+var updateLetterToGuess = function () {
 
-        }
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log(letterToGuess);
+};
+
+var updateGuessesSoFar = function () {
+
+    document.getElementById("guessSoFar").innerHTML = guessedLetters.join(", ");
+};
+
+function winA() {
+    if (wins === 3) {
+        alert("You Win!");
+        resetScore()
+    }
+}
+function lossA() {
+    if (losses === 5) {
+        alert("Take a L!");
+        resetScore()
+    }
+}
+function resetScore() {
+    wins = 0
+    losses = 0
+};
+
+var reset = function () {
+    guessesLeft = 20;
+    guessedLetters = [];
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+};
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+document.onkeydown = function (event) {
+
+    guessesLeft--;
+
+    var letter = event.key.toLowerCase();
+
+    guessedLetters.push(letter);
+
+    updateGuessesLeft();
+    updateGuessesSoFar();
+
+    if (letter === letterToGuess) {
+
+        wins++;
+        document.getElementById("Win").innerHTML = wins;
+        winA();
+        reset();
     }
 
-    document.getElementById("idWin").innerHTML = Wins;
-    document.getElementById("idLoss").innerHTML = Losses;
-    document.getElementById("idGuessesLeft").innerHTML = Guesses;
-    document.getElementById("idGuessSoFar").innerHTML = GuessesSoFar;
+    if (guessesLeft === 0) {
 
-}
+        losses++;
+        document.getElementById("Loss").innerHTML = losses;
+        lossA();
+        reset();
+    }
+};
